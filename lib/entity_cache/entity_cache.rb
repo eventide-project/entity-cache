4e162145b65acb8bb7f898c25867d3c6b15dfa09
ntl <nathanladd+github@gmail.com>
2017-07-05 13:02:34 -0500

    Logging is added to entity cache

--- lib/entity_cache/entity_cache.rb
+++ lib/entity_cache/entity_cache.rb
@@ -1,4 +1,6 @@
 class EntityCache
+  include Log::Dependency
+
   configure :entity_cache
 
   attr_writer :persist_interval
@@ -38,10 +40,18 @@ class EntityCache
   end
 
   def get(id)
+    logger.trace { "Get entity (ID: #{id.inspect})" }
+
     record = temporary_store.get(id)
 
     if record.nil?
       record = restore(id)
+    end
+
+    if record.nil?
+      logger.info { "Get entity failed; cache miss (ID: #{id.inspect}, #{Record::LogText.get(record)})" }
+    else
+      logger.info { "Get entity done (ID: #{id.inspect}, #{Record::LogText.get(record)})" }
     end
 
     record
@@ -50,31 +60,40 @@ class EntityCache
   def put(id, entity, version, time: nil, persisted_version: nil, persisted_time: nil)
     time ||= clock.now
 
+    updated_persistent_store = false
+
+    record = Record.build(id, entity, version, time)
+
+    logger.trace { "Put entity (ID: #{id.inspect}, #{Record::LogText.get(record)}, Persist Interval: #{persist_interval.inspect})" }
+
     if (version - persisted_version.to_i) >= persist_interval
       persistent_store.put(id, entity, version, time)
 
       persisted_version = version
       persisted_time = time
+      updated_persistent_store = true
     end
 
-    record = Record.build(
-      id,
-      entity,
-      version,
-      time,
-      persisted_version: persisted_version,
-      persisted_time: persisted_time
-    )
+    record.persisted_version = persisted_version
+    record.persisted_time = persisted_time
 
     temporary_store.put(record)
+
+    logger.info { "Put entity done (ID: #{id.inspect}, #{Record::LogText.get(record)}, Persist Interval: #{persist_interval.inspect}, Updated Persistent Store: #{updated_persistent_store})" }
 
     record
   end
 
   def restore(id)
+    logger.trace { "Restoring entity (ID: #{id.inspect})" }
+
     entity, version, time = persistent_store.get(id)
 
-    return nil if entity.nil?
+    if entity.nil?
+      logger.debug { "Could not restore entity (ID: #{id.inspect})" }
+
+      return nil
+    end
 
     record = Record.build(
       id,
@@ -86,6 +105,8 @@ class EntityCache
     )
 
     temporary_store.put(record)
+
+    logger.debug { "Restored entity (ID: #{id.inspect}, #{Record::LogText.get(record)})" }
 
     record
   end
