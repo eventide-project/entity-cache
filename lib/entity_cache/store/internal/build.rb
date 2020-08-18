7f49afde290919472ffece2e268627f19c192686
Nathan Ladd <nathanladd@gmail.com>
2020-08-18 18:15:48 -0500

    Use of Array#* is replaced with Array#join for MRuby compatibility

--- lib/entity_cache/store/internal/build.rb
+++ lib/entity_cache/store/internal/build.rb
@@ -18,7 +18,7 @@ class EntityCache
           scopes.fetch(scope) do
             *scopes, final_scope = self.scopes.keys
 
-            scope_list = "#{scopes.map(&:inspect) * ', '} or #{final_scope.inspect}"
+            scope_list = "#{scopes.map(&:inspect).join(', ')} or #{final_scope.inspect}"
 
             error_message = %{Scope #{scope.inspect} is unknown. It must be one of: #{scope_list}}
 
